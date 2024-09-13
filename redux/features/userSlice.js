4ac2babe408850ebe67ba5d2e2d8d6a95918cea8
realAhmedCodes <ahmedsiddque576@gmail.com>
2024-09-13 23:12:35 +0500

    Redux Code added

---
+++ redux/features/userSlice.js
@@ -0,0 +1,26 @@
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  userId: null,
+  sellerType: null,
+  sellerId: null,
+  email: null,
+  isVerified: false,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser(state, action) {
+      return { ...state, ...action.payload };
+    },
+    clearUser(state) {
+      return initialState;
+    },
+  },
+});
+
+export const { setUser, clearUser } = userSlice.actions;
+export default userSlice.reducer;
