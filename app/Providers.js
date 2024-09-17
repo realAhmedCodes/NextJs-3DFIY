711fd88a9d52afd50b1e4b0a9909c03c1da19117
realAhmedCodes <ahmedsiddque576@gmail.com>
2024-09-17 22:51:13 +0500

    Redux Persist added

--- app/Providers.js
+++ app/Providers.js
@@ -1,10 +1,21 @@
 "use client";
-
 import { Provider } from "react-redux";
 import store from "@/redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+// Create a persistor instance
+let persistor = persistStore(store);
 
 const Providers = ({ children }) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      {/* PersistGate delays rendering until the persisted state has been loaded */}
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 };
 
 export default Providers;
--- redux/store.js
+++ redux/store.js
@@ -1,11 +1,27 @@
-
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./features/userSlice"
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // default is localStorage for web
+import userReducer from "./features/userSlice";
 
+// Persist config
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+// Create a persisted reducer
+const persistedReducer = persistReducer(persistConfig, userReducer);
+
+// Create and export the store with persisted reducer and default middleware
 const store = configureStore({
   reducer: {
-    user: userReducer,
+    user: persistedReducer, // Ensure reducer is nested under user
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // This prevents errors with non-serializable data, such as functions
+    }),
 });
 
+export const persistor = persistStore(store);
 export default store;
