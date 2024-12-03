99845885df9033c45fa95bafa7a784a850785e43
realAhmedCodes <ahmedsiddque576@gmail.com>
2024-12-04 02:13:51 +0500

    Updated APIs

--- app/api/ratings/designers/[id]/postRatings/route.ts
+++ app/api/ratings/designers/[id]/postRatings/route.ts
@@ -1,6 +1,5 @@
 // app/api/ratings/[id]/route.ts
-// app/api/ratings/[id]/route.ts
-// app/api/ratings/[id]/route.ts
+
 
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
